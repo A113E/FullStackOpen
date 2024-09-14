be2212d9a4edfc7450110d3429632a8358620827
A113E <amg11amg2@gmail.com>
2024-09-14 17:14:23 -0400

    Add files via upload
    La app y el archivo main de los ejercicios 1.3-1.5

---
+++ Ejercicios 1.3-1.5.jsx
@@ -0,0 +1,69 @@
+//Componente Header Props
+const Header = (props) => {
+  console.log(props);
+  return ( 
+  <div>
+    <h1> {props.course.name} </h1>
+  </div>
+  )
+}
+
+
+//Componente Content Props
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p> {props.course.parts[0].name}  {props.course.parts[0].exercises} </p>
+      <p> {props.course.parts[1].name} {props.course.parts[1].exercises} </p>
+      <p> {props.course.parts[2].name} {props.course.parts[2].exercises} </p>
+    </div>
+  )
+}
+
+//Componente Total
+const Total = (props) => {
+  console.log(props);
+  return (
+    <p> <strong>Total number of exercises:</strong> {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} </p>
+  )
+}
+
+//Aplicación principal o raíz
+const App = () => {
+
+  //Array principal
+  const course = { 
+    name: 'Half Stack application development',
+  
+  parts:  [
+    {
+      name:"Fundamentals of React",
+      exercises: 10, 
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    } 
+  ]
+     }
+  
+  return (
+    <div>
+      <Header course ={course} />
+      <Content course ={course} />
+      <Total course ={course} />
+    </div>
+  )
+}
+
+
+
+
+// Exportación de la aplicación
+export default App
+
